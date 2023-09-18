fc6d1ed753f76cfc8c4c3d8672cf2af7775a63f4
ThanhTanPM2000 <thanhtanpm2000@gmail.com>
2023-09-19 01:43:59 +0700

    change server_url to production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,7 @@ module.exports = {
       env: {
         NODE_ENV: "development",
         RABBITMQ_URL: "amqp://tannguyen:514723@34.173.207.107:5672",
-        // SERVER_URL: "https://dev-api-academy.megalithinc.com",
-        SERVER_URL: "http://localhost:3333",
+        SERVER_URL: "https://dev-api-academy.megalithinc.com",
       },
     },
   ],
